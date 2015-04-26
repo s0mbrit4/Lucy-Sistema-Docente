849afbc11029d51cd54f6f3446d9d4eca6562bd1
s0mbrit4 <terrymicols@gmail.com>
2015-04-26 17:29:21 -0300

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0;' name='viewport' />
+</head>
+<body bgcolor=#E1DEDE>
+
+	<p align=center><ins><font color="red">Sistema Lucy</font></ins></p>
+
+<?php
+$fecha= date ("j-n-Y");
+echo "<p align=right>Fecha Actual: $fecha</p>";
+?> 
+	<p align=right><a href="./menudocente.php" ALT="Acceso Docente"><img src="./images/accesodocente.png" title="Acceso Docente"></img></a></p>	
+	<table>
+	<tr><td><img src="./images/Alumno.jpg" width="150" height="150"></img></td></tr>
+	<tr><td align=center><a href="./SelectLiceo.php">Ingreso Alumno</a></td></tr>
+	</table>
+<foot>
+<p align=bottom>Atencion en la version para celulares no funciona el boton volver</p>
+<!-Local Time Clock widget - HTML code - localtimes.info --><div align="right" style="margin:0px 0px 0px 0px"><div align="center" style="width:0px;border:1px solid #ccc;background:#fff ;color: #fff ;font-weight:bold"></div><script type="text/javascript" src="http://localtimes.info/clock.php?continent=South America&country=Uruguay&city=Montevideo&color=black-white&widget_number=1000"></script></div><!-end of code-->
+</foot>
+</body>
